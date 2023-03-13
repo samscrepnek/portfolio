0d35c376e8df07d8bc95aec75c79adaa561b440d
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-13 13:21:52 -0700

    added end point for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,37 @@
-import { useState, useEffect } from 'react'
-import Loading from './Loading'
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const Home = () => {
-    const restPath = ''
-    const [restData, setData] = useState([])
-    const [isLoaded, setLoadStatus] = useState(false)
+  const restPath = "https://samscrepnek.ca/qM3B3Db6DyVW5YPK/wp-json/wp/v2/pinkmug-project";
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(restPath)
-            if ( response.ok ) {
-                const data = await response.json()
-                setData(data)
-                setLoadStatus(true)
-            } else {
-                setLoadStatus(false)
-            }
-        }
-        fetchData()
-    }, [restPath])
-    
-    return (
-        <>
-        { isLoaded ?
-            <article>
-                <h1>{restData.title.rendered}</h1>
-                <div className="entry-content">
-                    
-                </div>
-            </article>
-        : 
-            <Loading />
-        }
-        </>   
-    )
-  
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
 
-export default Home
+  return (
+    <>
+      {isLoaded ? (
+        <article>
+          <h1>{restData.title.rendered}</h1>
+          <div className="entry-content"></div>
+        </article>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+};
+
+export default Home;
