c52cf2e611b0010c33590408bd2f928c28ee2a14
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-14 10:59:41 -0700

    added path to project post type

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Loading from "./Loading";
 
 const Home = () => {
-  const restPath = "https://samscrepnek.ca/qM3B3Db6DyVW5YPK/wp-json/wp/v2/pinkmug-project";
+  const restPath = "https://samscrepnek.ca/qM3B3Db6DyVW5YPK/wp-json/wp/v2/pinkmug-project?_embed&acf_format=standard";
   const [restData, setData] = useState([]);
   const [isLoaded, setLoadStatus] = useState(false);
 
@@ -23,10 +24,17 @@ const Home = () => {
   return (
     <>
       {isLoaded ? (
-        <article>
-          <h1>{restData.title.rendered}</h1>
-          <div className="entry-content"></div>
-        </article>
+        <div className="portfolio">
+          {restData.map((post) => (
+            <article key={post.id} id={`post-${post.id}`}>
+              <img src={`${post.acf.hero_img.url}`} alt=""></img>
+              <Link to={`/blog/${post.slug}`}>
+                <h2>{post.title.rendered}</h2>
+              </Link>
+              <p>{post.acf.description}</p>
+            </article>
+          ))}
+        </div>
       ) : (
         <Loading />
       )}
