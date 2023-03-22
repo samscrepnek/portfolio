47112a49e82429ab6dd379364f293378088a250b
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-22 14:10:56 -0700

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,49 +23,6 @@ const Project = () => {
     fetchData();
   }, [restPath]);
 
-  // Checks if item(string and arrays) has content.
-  const checkACFContent = function (item) {
-    // Checks if string and arrays have content
-    const CheckItemLength = function (item) {
-      let itemLength = item.length;
-      if (itemLength > 0) {
-        // item has content
-        return true;
-      } else {
-        // item has no content
-        return false;
-      }
-    };
-
-    // Checks type of item
-    let itemType = typeof item;
-    if (itemType === "object") {
-      if (!Array.isArray(item)) {
-        // item is an object with content
-        return true;
-      } else {
-        if (CheckItemLength(item)) {
-          // array has content
-          return true;
-        } else {
-          // array has no content
-          return false;
-        }
-      }
-    } else if (itemType === "string") {
-      if (CheckItemLength(item)) {
-        // string has content
-        return true;
-      } else {
-        // string has no content
-        return false;
-      }
-    } else {
-      // item has no content
-      return false;
-    }
-  };
-
   return (
     <>
       {isLoaded ? (
@@ -81,7 +38,7 @@ const Project = () => {
             <p>{restData.acf.description}</p>
           </div>
 
-          {checkACFContent(restData.acf.skills_used) ? (
+          {restData.acf.skills_used && restData.acf.skills_used.length > 0 && (
             <div>
               <h2>Skills Used</h2>
               <ul>
@@ -90,11 +47,9 @@ const Project = () => {
                 ))}
               </ul>
             </div>
-          ) : (
-            <></>
           )}
 
-          {checkACFContent(restData.acf.features) ? (
+          {restData.acf.features && (
             <div>
               <h2>Features</h2>
               {restData.acf.features.map((feature) => (
@@ -105,17 +60,13 @@ const Project = () => {
                 </div>
               ))}
             </div>
-          ) : (
-            <></>
           )}
 
-          {checkACFContent(restData.acf.takeaways) ? (
+          {restData.acf.takeaways && (
             <div>
               <h2>Takeaways</h2>
               <p>{restData.acf.takeaways}</p>
             </div>
-          ) : (
-            <></>
           )}
 
           <div>
