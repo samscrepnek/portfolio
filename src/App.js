029d7eccd1f6eb10519e68fe072cff24347d0dbd
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-29 12:03:00 -0700

    Began styling on project page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Project from "./components/Post";
+import Project from "./components/Project";
 import Contact from "./components/Contact";
 
 function App() {
