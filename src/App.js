461a3c6faf3ae310176620278db26d7b8157ddb2
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-30 15:13:12 -0700

    removed unused code

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ import Project from "./components/Project";
 import Contact from "./components/Contact";
 
 function App() {
-  // const featuredImage = (featuredImageObject) => {
-  //   let imgWidth = featuredImageObject.media_details.sizes.full.width;
-  //   let imgHeight = featuredImageObject.media_details.sizes.full.height;
-  //   let imgURL = featuredImageObject.source_url;
-  //   let img = `<img src="${imgURL}"
-  //       width="${imgWidth}"
-  //       height="${imgHeight}"
-  //       alt="${featuredImageObject.alt_text}"
-  //       srcset="${imgURL} ${imgWidth}w,
-  //       ${featuredImageObject.media_details.sizes.large ? featuredImageObject.media_details.sizes.large.source_url + " 1024w," : ""}
-  //       ${featuredImageObject.media_details.sizes.medium_large ? featuredImageObject.media_details.sizes.medium_large.source_url + " 768w," : ""}
-  //       ${featuredImageObject.media_details.sizes.medium ? featuredImageObject.media_details.sizes.medium.source_url + " 300w" : ""}"
-  //       sizes="(max-width: ${imgWidth}) 100vw, ${imgWidth}px">`;
-  //   return { __html: img };
-  // };
-
   useEffect(() => {
     window.addEventListener("click", (event) => {
       const nav = document.querySelector(".site-navigation");
