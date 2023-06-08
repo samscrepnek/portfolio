13ef96231d9bc06e8a1338588a458eac5d390548
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-06-08 11:17:31 -0700

    Changed home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,38 +26,47 @@ const Home = () => {
   return (
     <>
       {isLoaded ? (
-        <div className="page-wrapper featured-works">
-          <section>
-            <h2>Recent Work</h2>
-            {restData.slice(0, 3).map((post) => (
-              <article key={post.id} id={`post-${post.id}`} className="project">
-                <div className="project-content-wrapper">
-                  <Link to={`/${post.slug}`}>
-                    {post.acf.mobile_hero.url ? (
-                      <picture>
-                        <source media="(min-width: 650px)" srcSet={`${post.acf.hero_img.url}`} />
-                        <img src={`${post.acf.mobile_hero.url}`} alt={`${post.acf.mobile_hero.alt}`}></img>
-                      </picture>
-                    ) : (
-                      <img src={`${post.acf.hero_img.url}`} alt={`${post.acf.hero_img.alt}`}></img>
-                    )}
-                  </Link>
-                  <Link to={`/${post.slug}`}>
-                    <h3>{post.title.rendered}</h3>
-                  </Link>
-                  <div className="project-overview">
-                    <p>{post.acf.project_overview}</p>
-                  </div>
-                  <Link to={`/${post.slug}`}>
-                    <button className="project-btn">View Full Project Page</button>
-                  </Link>
-                </div>
-              </article>
-            ))}
-          </section>
-          <section id="home-contact">
-            <Contact />
-          </section>
+        <div className="page-wrapper">
+          <div className="section-wrapper recent-works">
+            <section>
+              <h2>Recent Work</h2>
+              <div className="home-works">
+                {restData.slice(0, 3).map((post) => (
+                  <article key={post.id} id={`post-${post.id}`} className="project">
+                    <div className="project-content-wrapper">
+                      <Link to={`/${post.slug}`}>
+                        {post.acf.mobile_hero.url ? (
+                          <picture>
+                            <source media="(min-width: 650px)" srcSet={`${post.acf.hero_img.url}`} />
+                            <img src={`${post.acf.mobile_hero.url}`} alt={`${post.acf.mobile_hero.alt}`}></img>
+                          </picture>
+                        ) : (
+                          <img src={`${post.acf.hero_img.url}`} alt={`${post.acf.hero_img.alt}`}></img>
+                        )}
+                      </Link>
+                      <Link to={`/${post.slug}`}>
+                        <h3>{post.title.rendered}</h3>
+                      </Link>
+                      <div className="project-overview">
+                        <p>{post.acf.project_overview}</p>
+                      </div>
+                      <Link to={`/${post.slug}`}>
+                        <button className="project-btn">View Full Project Page</button>
+                      </Link>
+                    </div>
+                  </article>
+                ))}
+              </div>
+            </section>
+            <section className="home-cta">
+              <div className="cta-wrapper">
+                <Link to="/portfolio">
+                  <button>View Portfolio</button>
+                </Link>
+              </div>
+            </section>
+          </div>
+          <Contact />
         </div>
       ) : (
         <Loading />
