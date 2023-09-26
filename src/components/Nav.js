577da19aa97be95922a1c8f3625d0cc73470d638
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-09-25 18:10:09 -0700

    added module to disable scrolling while nav menu is open

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+// disableScroll module from https://github.com/gilbarbara/disable-scroll  //
+import disableScroll from "disable-scroll";
 import CopyEmail from "./CopyEmail";
 
 const Nav = () => {
@@ -6,6 +8,16 @@ const Nav = () => {
     const nav = document.querySelector(".nav-menu-container");
     nav.classList.toggle("hidden");
   }
+
+  const openMenu = () => {
+    toggleMenu();
+    disableScroll.on();
+  };
+
+  const closeMenu = () => {
+    toggleMenu();
+    disableScroll.off();
+  };
 
   return (
     <nav className="top-nav">
@@ -17,7 +29,7 @@ const Nav = () => {
             </NavLink>
           </li>
           <li>
-            <button aria-label="Open menu" className="menu-icon" onClick={toggleMenu}>
+            <button aria-label="Open menu" className="menu-icon" onClick={openMenu}>
               <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M24 6h-24v-3h24v4zm0 5h-24v3h24v-4zm0 8h-24v3h24v-4z" />
               </svg>
@@ -26,19 +38,19 @@ const Nav = () => {
         </ul>
         <div className="nav-menu-container hidden">
           <div className="nav-menu-wrapper">
-            <button aria-label="Close menu" className="close-icon" onClick={toggleMenu}>
+            <button aria-label="Close menu" className="close-icon" onClick={closeMenu}>
               <svg aria-hidden="true" focusable="false" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" width="32" height="32" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
               </svg>
             </button>
             <menu className="nav-menu">
               <li>
-                <NavLink aria-label="Link to home page" to="/" onClick={toggleMenu}>
+                <NavLink aria-label="Link to home page" to="/" onClick={closeMenu}>
                   Home
                 </NavLink>
               </li>
               <li>
-                <NavLink aria-label="Link to about page" to="/about" onClick={toggleMenu}>
+                <NavLink aria-label="Link to about page" to="/about" onClick={closeMenu}>
                   About
                 </NavLink>
               </li>
