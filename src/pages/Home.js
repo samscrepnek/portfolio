9b5fd4904d67a5b96c85f1c57ffda46a7c2c3464
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2024-03-22 13:38:01 -0700

    updated home style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,55 +32,63 @@ const Home = () => {
       </Helmet>
       {isLoaded ? (
         <div className="page-wrapper">
-          <div className="home">
+          <div id="home">
             <header id="home-header">
-              <div className="info">
+              <div id="home-header-info">
                 <p>Hi I'm</p>
                 <h1>Sam Screpnek</h1>
               </div>
               {restData.acf.main_image.url && (
-                <div className="intro-pic">
+                <div id="home-header-pic">
                   <picture>
                     <img aria-hidden="true" focusable="false" src={`${restData.acf.main_image.url}`} alt={`${restData.acf.main_image.alt}`}></img>
                   </picture>
                 </div>
               )}
-              <div>
-                <p>A Web Developer specializing in web design, development, and SEO.</p>
-              </div>
             </header>
-            <section className="about-cta">
-              <Link
-                aria-label="about-link"
-                to="/about"
-                onClick={() => {
-                  window.scroll(0, 0);
-                }}
-              >
-                <button>
-                  <p>Read about my story</p>
-                </button>
-              </Link>
-            </section>
-            {restData.acf.featured_project.ID && (
-              <section id="home-featured-section">
-                <p>Check out my recent project</p>
-                <FeaturedProject postID={restData.acf.featured_project.ID} />
+            <div id="home-flex-div">
+              <section id="home-blurb">
+                <p>A Web Developer specializing in web design, development, and SEO.</p>
               </section>
-            )}
-            <section className="portfolio-cta">
-              <Link
-                aria-label="portfolio-link"
-                to="/portfolio"
-                onClick={() => {
-                  window.scroll(0, 0);
-                }}
-              >
-                <button>
-                  <p>Or look at my full portfolio</p>
-                </button>
-              </Link>
-            </section>
+              <section className="about-cta">
+                <Link
+                  aria-label="about-link"
+                  to="/about"
+                  onClick={() => {
+                    window.scroll(0, 0);
+                  }}
+                >
+                  <button>
+                    <p>Read about my story </p>
+                    <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero" />
+                    </svg>
+                  </button>
+                </Link>
+              </section>
+              {restData.acf.featured_project.ID && (
+                <section id="home-featured-section">
+                  <p>Check out my recent project</p>
+                  <FeaturedProject postID={restData.acf.featured_project.ID} />
+                </section>
+              )}
+              <section className="portfolio-cta">
+                <Link
+                  aria-label="portfolio-link"
+                  to="/portfolio"
+                  onClick={() => {
+                    window.scroll(0, 0);
+                  }}
+                >
+                  <button>
+                    <p>Or look at my full portfolio</p>
+                    <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero" />
+                    </svg>
+                  </button>
+                </Link>
+              </section>
+            </div>
           </div>
         </div>
       ) : (
